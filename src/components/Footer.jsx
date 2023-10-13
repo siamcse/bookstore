e1a207af1233a6b387ab8741c00b31bf2d3a581a
Abu Siam <siamcse01@gmail.com>
2023-10-14 00:06:09 +0600

    books page added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,24 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='min-h-screen bg-black'>
-            <div className='container mx-auto pt-16'>
-                <h1 className='text-3xl font-bold text-white'>If you have any queries feel free to ask here.</h1>
-                <p class="text-white">© 2021 - All rights reserved.</p>
+            <div className='container mx-auto '>
+                <h1 className='text-3xl font-bold text-white py-16'>If you have any queries feel free to ask here.</h1>
+                <div>
+                    <div className='mb-6'>
+                        <p className='text-white mb-2 font-medium'>Name:</p>
+                        <input type="text"  className='w-5/6 sm:w-1/2 py-4 rounded outline-none px-3' placeholder='Enter your Name'/>
+                    </div>
+                    <div className='mb-6'>
+                        <p className='text-white mb-2 font-medium'>Email:</p>
+                        <input type="email" className='w-5/6 sm:w-1/2 py-4 rounded outline-none px-3' placeholder='Enter your Email'/>
+                    </div>
+                    <div className='mb-6'>
+                        <p className='text-white mb-2 font-medium'>Query:</p>
+                        <textarea type="text" className='w-5/6 sm:w-1/2 py-4 rounded outline-none px-3' placeholder='Type your Query'/>
+                    </div>
+                </div>
+                <button className='py-2 px-6 bg-primary rounded text-white'>Submit</button>
+                <p class="text-white mt-10">© 2023 BookStore - All rights reserved.</p>
             </div>
         </footer>
     );
--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchField = () => {
     return (
         <div className='mt-5'>
-            <div className='py-3 px-3 flex bg-white rounded shadow-xl shadow-dark'>
+            <div className='py-3 px-3 flex bg-white rounded shadow shadow-gray'>
                 <input type="text" className=' px-3 py-1 outline-none w-full' placeholder='Search Books' />
                 <button className='bg-primary text-white px-3 rounded'>Search</button>
             </div>
