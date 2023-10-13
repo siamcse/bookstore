357ade804f92f2dc3dd244dcb918c30d45ae8521
Abu Siam <siamcse01@gmail.com>
2023-10-14 00:30:41 +0600

    signup page added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
+import authBg from '../../assets/auth-bg.jpg';
 
 const SignUp = () => {
     return (
-        <div>
-            This is signup page
+        <div className='relative'>
+            <Navbar darkText={true} />
+            <div className='grid grid-cols-2 gap-20'>
+                <div className='min-h-screen' style={{ backgroundImage: `url(${authBg})` }}>
+                </div>
+                <div className='flex flex-col justify-center min-h-screen'>
+                    <h1 className='text-3xl font-bold'>Sign Up</h1>
+                    <p>Create a new account with email and password.</p>
+                    <div className='mt-10'>
+                        <div className='mb-6'>
+                            <p className='mb-1 font-medium'>Username</p>
+                            <input type="text" className='w-5/6 sm:w-3/4 py-4 border-2 rounded outline-none px-3' placeholder='Enter your username' />
+                        </div>
+                        <div className='mb-6'>
+                            <p className='mb-1 font-medium'>Email</p>
+                            <input type="email" className='w-5/6 sm:w-3/4 py-4 border-2 rounded outline-none px-3' placeholder='Enter your email' />
+                        </div>
+                        <div className='mb-6'>
+                            <p className='mb-1 font-medium'>Password</p>
+                            <input type="password" className='w-5/6 sm:w-3/4 py-4 border-2 rounded outline-none px-3' placeholder='Enter your password' />
+                        </div>
+                        <button className='py-3 px-6 bg-primary text-white rounded'>Login</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
